4b74cd343c0e2e43ab02e45a0017443824c34034
NikaEliashvili <nikaeliashvili18@outlook.com>
2023-09-24 18:16:35 +0400

    Update prev and next page buttons

--- src/componenets/PagesChangingBtns.jsx
+++ src/componenets/PagesChangingBtns.jsx
@@ -12,12 +12,12 @@ const PagesChangingBtns = memo(function PagesChangingBtns({
     <div className="text-orange-600 flex justify-center items-center gap-5 w-full py-4 ">
       <button
         onClick={decPage}
-        className="flex flex-row text-center justify-center items-center text-2xl font-bold font-mono  bg-zinc-800 cursor-pointer rounded-sm transition duration-100 enabled:hover:bg-orange-900 enabled:hover:text-gray-300 enabled:active:scale-[0.97] disabled:text-zinc-600  disabled:cursor-default"
+        className="group flex flex-row text-center justify-center items-center text-2xl font-bold font-mono  bg-zinc-800 cursor-pointer rounded-sm transition duration-100 enabled:hover:bg-orange-900 enabled:hover:text-gray-300 enabled:active:scale-[0.97] disabled:text-zinc-600  "
         disabled={curPage === 1}
       >
         <a
           href="#topBorder"
-          className="w-full flex flex-row text-center justify-center items-center py-1 pr-5 pl-2 scroll-smooth cursor-default"
+          className="w-full flex flex-row text-center justify-center items-center py-1 pr-5 pl-2 scroll-smooth group-disabled:cursor-default group:cursor-pointer "
         >
           <RiArrowLeftSFill className="text-4xl" />
           Prev
@@ -26,12 +26,12 @@ const PagesChangingBtns = memo(function PagesChangingBtns({
       <h1 className="text-xl font-mono font-bold">{curPage}</h1>
       <button
         onClick={incPage}
-        className="flex flex-row text-center justify-center items-center text-2xl font-bold font-mono  bg-zinc-800 cursor-pointer rounded-sm transition duration-100 enabled:hover:bg-orange-900 enabled:hover:text-gray-300 enabled:active:scale-[0.97] disabled:text-zinc-600 disabled:cursor-default"
+        className="group flex flex-row text-center justify-center items-center text-2xl font-bold font-mono  bg-zinc-800 cursor-pointer rounded-sm transition duration-100 enabled:hover:bg-orange-900 enabled:hover:text-gray-300 enabled:active:scale-[0.97] disabled:text-zinc-600"
         disabled={error || curPage === maxPage}
       >
         <a
           href="#topBorder"
-          className="w-full flex flex-row text-center justify-center items-center py-1 pl-5 pr-2 scroll-smooth cursor-default"
+          className="w-full flex flex-row text-center justify-center items-center py-1 pl-5 pr-2 scroll-smooth  group-disabled:cursor-default group:cursor-pointer"
         >
           Next
           <RiArrowRightSFill className="text-4xl" />
